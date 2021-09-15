82b831e2b7d3d65b2340ebb10066d778ae3b6d1a
Senpai <augustus.writer@gmail.com>
2021-09-15 18:33:16 +0100

    added file write out

--- wordlist_generator.py
+++ wordlist_generator.py
@@ -1,22 +1,24 @@
 import string
 from random import *
 
-passwords = ["WORDS","GO","HERE"] #words can be scraped from sites using CEWL tool
+filename = 'banned-passwords.txt'
 characters = string.ascii_letters + string.punctuation  + string.digits
 
 for p in passwords:
     password =  p.join(choice(characters) for x in range(randint(1, 6)))
-    print password
+    with open(filename, 'a') as file_object:
+        file_object.write(password + "\n")
 
 for p in passwords:
     password2 = "".join(choice(characters) + p for x in range(randint(1, 6)))
-    print password2
+    with open(filename, 'a') as file_object:
+        file_object.write(password2 + "\n")
 
 
 for l in passwords:
     password3 = l + choice(string.punctuation) + choice(string.digits) + choice(string.digits) + choice(string.digits)
-    #password3 = l.join(choice(string.punctuation)) + choice(string.digits)
-    print password3
+    with open(filename, 'a') as file_object:
+        file_object.write(password3 + "\n")
 
 
 
